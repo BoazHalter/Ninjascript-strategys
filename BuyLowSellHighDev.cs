334bc231e48c93aae0521a266c37ce07dd69fd6a
Boaz Halter <30419068+BoazHalter@users.noreply.github.com>
2024-04-28 14:50:48 +0300

    Update BuyLowSellHighDev.cs

--- BuyLowSellHighDev.cs
+++ BuyLowSellHighDev.cs
@@ -1,6 +1,4 @@
 
-
-
 #region Using declarations
 using System;
 using System.Collections.Generic;
@@ -30,6 +28,8 @@ namespace NinjaTrader.NinjaScript.Strategies
 {
 	public class BuyLowSellHighDev : Strategy
 	{
+		private Order stopLossOrder = null;
+		private Order entryOrder = null;
 		private int barLength = 3; // Number of minutes per bar
         private int entryTicksAboveHigh = 5;
         private int entryTicksBelowLow = 5;
@@ -37,7 +37,9 @@ namespace NinjaTrader.NinjaScript.Strategies
 		private int lastThreeTrades 		= 0;  	// This variable holds our value for how profitable the last three trades were.
 		private int priorNumberOfTrades 	= 0;	// This variable holds the number of trades taken. It will be checked every OnBarUpdate() to determine when a trade has closed.
 		private int priorSessionTrades		= 0;	// This variable holds the number of trades taken prior to each session break.
-
+		private bool changedDirection;
+		double plTotalStatus ;
+		double plUnrealized ;
 		
 		protected override void OnStateChange()
 		{
@@ -63,10 +65,12 @@ namespace NinjaTrader.NinjaScript.Strategies
 				BarsRequiredToTrade							= 20;
 				StopLossTicks				   				= 30;
 				ProfitTargetTicks			    			= 100;
+				
 				//AddChartIndicator(Indicator.HighLowAverage, Periods[barLength], LineStyles.Solid, Colors.Gray);
 				// Disable this property for performance gains in Strategy Analyzer optimizations
 				// See the Help Guide for additional information
-				IsInstantiatedOnEachOptimizationIteration	= true;
+				//IsInstantiatedOnEachOptimizationIteration	= true;
+				//changedDirection 							= false;
 				
                 
 			}
@@ -74,82 +78,198 @@ namespace NinjaTrader.NinjaScript.Strategies
 			else if (State == State.Configure)
 			{
 				IsEnabled = true;
-				SetStopLoss(CalculationMode.Ticks, StopLossTicks);
-				SetProfitTarget("Long 1a",CalculationMode.Ticks, 10);
-				SetProfitTarget("Long 1b",CalculationMode.Ticks, 15);
-				SetProfitTarget("Long 1c",CalculationMode.Ticks, ProfitTargetTicks);
+				//SetTrailStop(CalculationMode.Ticks,40);
 
 			}
 		}
+		
 		protected override void OnBarUpdate()
 		{
-			if (CurrentBar < BarsRequiredToTrade)
-                return;
-			log("Real Time Trades LosingTrades: " + SystemPerformance.RealTimeTrades.LosingTrades.TradesCount  );
-			log("AllTrades Losing Trades: " + SystemPerformance.AllTrades.LosingTrades.TradesCount );
+			if (State == State.Historical)
+    		  return;
 			
-			// Check for losing trade
-	        if (SystemPerformance.RealTimeTrades.LosingTrades.TradesCount <= 3)
-	        {
-	            log("LosingTrades: " + SystemPerformance.RealTimeTrades.LosingTrades.TradesCount ); 
-				entry();
+//			if(Historical)
+//			  return;		
+			
+			if (CurrentBar < BarsRequiredToTrade)
+	          return;
+			
+			try{
+			
+				CloseErrorWindows();
+				
+				plTotalStatus = Position.GetUnrealizedProfitLoss(PerformanceUnit.Currency, Close[0]) + SystemPerformance.AllTrades.TradesPerformance.NetProfit ;
+				plUnrealized = Position.GetUnrealizedProfitLoss(PerformanceUnit.Currency, Close[0]);
+				log("Real Time Trades LosingTrades: " + SystemPerformance.RealTimeTrades.LosingTrades.TradesCount  );
+				log("Current Strategy profit: " + plTotalStatus);
+				log("Open Position: " + plUnrealized);
+				log("Position.AveragePrice: "+ Position.AveragePrice);
+				
+					
+				
+				if((plTotalStatus > -100.0) && (plUnrealized > -100.0))
+					order(this.GetType().Name);
+				
+				if((plTotalStatus < -100.0) || (plUnrealized < -100.0))
+					log("Test");
+					//ExitLong();
+					
 				
 				// Resets the stop loss to the original value when all positions are closed
 				if (Position.MarketPosition == MarketPosition.Flat)
 				{
-					SetStopLoss(CalculationMode.Ticks, StopLossTicks);
+					SetTrailStop(CalculationMode.Ticks,40);
 				}
 				
 				// If a long position is open, allow for stop loss modification to breakeven
-				else if (Position.MarketPosition == MarketPosition.Long)
+				if (Position.MarketPosition == MarketPosition.Long)
 				{
 					// Once the bid price is greater than entry price+10 ticks, set stop loss to breakeven
-					if (GetCurrentBid(0) > Position.AveragePrice + 10 * TickSize)
-					{
-						SetStopLoss("Long 1b",CalculationMode.Price, Position.AveragePrice,false);
-						SetStopLoss("Long 1c",CalculationMode.Price, Position.AveragePrice,false);
+					if (GetCurrentBid(0) >= Position.AveragePrice + 7 * TickSize && Position.Quantity >= 1){
+						
+						log("Selling "+ (Position.Quantity -1) + " share at: " + Position.AveragePrice.ToString());
+						
+						SetProfitTarget("Long 1 Prtial", CalculationMode.Ticks, 6);
+						SetStopLoss("Long 1 Runner",CalculationMode.Price, Position.AveragePrice + 2 * TickSize,false );
+						//ExitLongStopLimit(GetCurrentBid(0),GetCurrentBid(0),"Partial Long 1a","Long 1 Prtial");
+						
 					}
 				}
-		        }           
-		        else
-		        {
-		            log("LosingTrades:" + SystemPerformance.AllTrades.LosingTrades.Count );
-					log("Reached 3 consecutive losing trades. Stopping strategy...");
-					log("Susspended");
-					
-		        }
-		}
-		private void entry()
-		{
-			if (Instrument.FullName.ToString().Contains("MES"));
-			{
-				log("Submitting Entry orders for: " + Instrument.FullName.ToString());
-				
-				//es entry
-				log("Submmit Long 1a share at: " + (GetCurrentBid(0) - TickSize *2).ToString());
-				EnterLongLimit(2,GetCurrentBid(0) - TickSize *2 ,"Long 1a");
-				
-				log("Submmit Long 1b share at: " + (GetCurrentBid(0) - TickSize *6).ToString());
-				EnterLongLimit(1, GetCurrentBid(0) - TickSize * 6, "Long 1b");
-				
-				log("Submmit Long 1c share at: " + (Low[1] - TickSize * 13).ToString());
-				EnterLongLimit(1, Low[1] - TickSize * 14, "Long 1c");
-
+	
 			}
-				
-			if (Instrument.FullName.ToString().Contains("MNQ"));
+			catch (Exception e)
 			{
-           		log("Submitting Entry orders for: " + Instrument.FullName.ToString());				
-				//nq enty
-				EnterLongLimit(2, Low[1] - TickSize * 9,  "Long 1a");
-				EnterLongLimit(1, Low[1] - TickSize * 13, "Long 1b");
-				EnterLongLimit(1, Low[1] - TickSize * 18, "Long 1c");
-			}	
+				// In case the indicator has already been Terminated, you can safely ignore errors
+				if (State >= State.Terminated)
+					return;
+
+				/* With our caught exception we are able to generate log entries that go to the Control Center logs and also print more detailed information
+				about the error to the Output Window. */
+				
+				// Submits an entry into the Control Center logs to inform the user of an error				
+				log("SampleTryCatch Error: Please check your  errors.");
+				
+				// Prints the caught exception in the Output Window
+				log(Time[0] + " " + e.ToString());
+				CloseStrategy("Close Strategy");
+			}
 		}
 		
-		private void exit(){
+		private void order(string strategy){
 			
+			if(Instrument.FullName.ToString().Contains("NQ")){							
+				log("Submitting Entry orders for: " + Instrument.FullName.ToString());				
+				if(strategy == "BuyLowSellHighDev"){
+					if (entryOrder == null)
+						//nq enty
+					
+						log("Submitting Long 1a quantity of 1 contract at: " + (GetCurrentBid(0) - TickSize * 2).ToString());
+						EnterLongLimit(1,GetCurrentBid(0) - TickSize * 5,  "Long 1 Prtial");
+						
+						EnterLongLimit(1,GetCurrentBid(0) - TickSize * 5,  "Long 1 Runner");
+					    
+					    // Only enter if at least 10 bars has passed since our last entry
+    					//if ((BarsSinceEntryExecution() > 2 || BarsSinceEntryExecution() == -1))
+						if (Position.MarketPosition == MarketPosition.Long)
+        					EnterLongLimit(2,Position.AveragePrice - 16 * TickSize,"Long Market 1ap");
+
+						// Only enter if at least 10 bars has passed since our last entry
+    					//if ((BarsSinceEntryExecution() > 4 || BarsSinceEntryExecution() == -1))
+						if (Position.MarketPosition == MarketPosition.Long)
+        					EnterLongLimit(3,Position.AveragePrice - 32 * TickSize,"Long Market 2ap");
+						
+					
+//						log("Submitting Long 1ap quantity of 1 contract at: " + (GetCurrentBid(0) - TickSize * 4).ToString());
+//						EnterLongLimit(1,GetCurrentBid(0) - TickSize * 6,  "Long 1ap");
+			
+//						ExitLongStopLimit(1,Position.AveragePrice + 8 * TickSize,Position.AveragePrice + 8 * TickSize,"Long 1a partial","Long 1ap");
+//						SetProfitTarget("Long 1ap", CalculationMode.Ticks, 12);
+//						log("Submitting Long 1b quantity of 1 contract at: " + (Low[1] - TickSize * 13).ToString());
+//						EnterLongLimit(1, Low[1] - TickSize * 25 , "Long 1b");
+				
+//						log("Submitting Long 1c quantity of 1 contract at: " + (Low[1] - TickSize * 18).ToString());
+//						EnterLongLimit(1, Low[0] - TickSize * 69, "Long 1c");
+//						log(System.Reflection.MethodBase.GetCurrentMethod().Name);
+					
+//					if (stopLossOrder == null)
+//						log((Position.Quantity).ToString() +" "+(Position.AveragePrice - 30 * TickSize).ToString());
+//    					//stopLossOrder = ExitLongStopMarket(Position.Quantity,Position.AveragePrice - 30 * TickSize);
+				}	
+			}
+			if(Instrument.FullName.ToString().Contains("ES")){
+				
+				log("Submitting Entry orders for: " + Instrument.FullName.ToString());
+				if (entryOrder == null)
+					//es entry
+					log("Submmit Long 1a share at: " + (GetCurrentBid(0) - TickSize *2).ToString());
+					EnterLongLimit(1,GetCurrentBid(0) - TickSize *2 ,"Long 1a");
+					
+					log("Submmit Long 1b share at: " + (GetCurrentBid(0) - TickSize *6).ToString());
+					EnterLongLimit(2, GetCurrentBid(0) - TickSize * 6, "Long 1b");
+					
+					log("Submmit Long 1c share at: " + (Low[1] - TickSize * 13).ToString());
+					EnterLongLimit(3, GetCurrentBid(0) - TickSize * 14, "Long 1c");
+					
+					log("Submmit: ExitLongStopLimit()");  
+				    ExitLongLimit(1,Position.AveragePrice + TickSize *40, "Partial 1a","Long 1a");
+				
+				if (stopLossOrder == null)
+    				stopLossOrder = ExitLongStopMarket(Position.Quantity,Position.AveragePrice - 30 * TickSize);		
+		    }
+	     
+					 
 		}
+		
+		protected override void OnOrderUpdate(Order order, double limitPrice, double stopPrice, int quantity, int filled, double averageFillPrice,
+                                    OrderState orderState, DateTime time, ErrorCode error, string nativeError)
+		{
+		  // Assign stopLossOrder in OnOrderUpdate() to ensure the assignment occurs when expected.
+		  // This is more reliable than assigning Order objects in OnBarUpdate,
+		  // as the assignment is not guaranteed to be complete if it is referenced immediately after submitting
+		  if (order.Name.Contains("Long 1") && orderState == OrderState.Filled)
+		    stopLossOrder = order;
+		 
+		  if (stopLossOrder != null && stopLossOrder == order)
+		  {
+		    // Rejection handling
+		    if (order.OrderState == OrderState.Rejected)
+		    {
+				log(order.OrderState.ToString() + "==" + OrderState.Rejected.ToString());
+				log("order: " + order.Name + "Cancelled");
+		        // Stop loss order was rejected !!!!
+		        // Do something about it here
+		    }
+		  }
+		}
+		protected void CloseErrorWindows()
+		{
+			if (!Dispatcher.CheckAccess())
+			{
+				Application.Current.Dispatcher.BeginInvoke(new Action(() => {
+					foreach (Window window in Application.Current.Windows)
+					{
+						if (window.Title == "Error")
+						{
+							window.Close();
+						}
+					}
+				}));
+				return;
+			}
+		}
+		
+		private void theWolf(){ //Cleaning after the mess
+			log("theWolf()");
+			if(Position.MarketPosition == MarketPosition.Short)
+			{
+				ExitShort(Position.Quantity); 
+			}
+			if(Position.MarketPosition == MarketPosition.Long)
+			{
+				ExitLong(Position.Quantity);
+			}
+		}		
+		
 		
 		private void log(string message)
 		{
